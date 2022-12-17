2e3dd14eb26e7d024c4ac2613b45195b0693a66c
Kenneth Salanga <kennethsalanga@gmail.com>
2022-12-17 02:51:22 -0500

    Day 1 Complete: - Fixed parsing by first trimming the string before converting to integer when mapping.

--- src/bin/day_1.rs
+++ src/bin/day_1.rs
@@ -9,12 +9,12 @@ fn main() {
     let input_calories = fs::read_to_string("../../inputs/day_1/example1.txt").unwrap();
     // Will panic, file opening error unhandled
 
-    let elves_calories = input_calories.split("\n\r");
+    let elves_calories = input_calories.split("\n\r\n");
 
     elves_calories.for_each(|elf_calories| {
         let elf_total_calories: i32 = elf_calories
             .split("\n")
-            .map(|calorie: &str| calorie.parse::<i32>().unwrap())
+            .map(|calorie: &str| calorie.trim_end().parse::<i32>().unwrap_or(0))
             .sum();
 
         elves_total_calories.push(elf_total_calories);
