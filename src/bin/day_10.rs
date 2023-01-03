6bc7ccb97553c8d5c60dbcac3ae2a5395c4ba71e
Kenneth Salanga <kennethsalanga@gmail.com>
2023-01-02 19:51:40 -0500

    day_10 initial pseudocode

---
+++ src/bin/day_10.rs
@@ -0,0 +1,46 @@
+// struct CPU
+// field:
+// cycle: usize
+// x_register: i32
+
+// method:
+// execute(instruction: str)
+// case addx:
+// cycle += 1
+// send (cycle, x_register) message
+// cycle += 1
+// x_register is new value
+// send (cycle, x_register) message
+// emit cycle:
+// case noop:
+// cycle += 1
+// send (cycle, x_register) message
+
+// I wanna use threads
+// std::sync::mpsc guarantees that messages sent by the sender will be received in order by the receiver.
+
+// Main transmitter Thread:
+// create the mpsc channel
+// spawns the receiver thread
+// and give the receiver thread ownership of receiver on mpsc channel
+
+// creates CPU struct
+// reads lines from file,
+// CPU struct will send to receiver (cycle, x_register) tuple during execute() of those lines.
+
+// wait for the receiver thread to close.
+
+// Signal Strength receiver Thread:
+// for each item received:
+// receives tuple of (usize, i32) representing the cycle and register value.
+// signal strength = 0.
+// if cycle is >= 220
+// break out
+// sets i = 0.
+// if cycle value is % 20 + 40i.
+// increment i by 1
+// add register value to signal strength
+
+fn main() {
+    todo!()
+}
