5bb53ec672031dd73c5a138af0bcd9d076a9cbf4
Kenneth Salanga <kennethsalanga@gmail.com>
2023-01-02 20:52:36 -0500

    signal strength receiver implemented:
    while it's still receiveing the messages:
    
    grab cpu cycle and x register from message
    
    update signal_strength when cycle condition is met

--- src/bin/day_10.rs
+++ src/bin/day_10.rs
@@ -2,48 +2,6 @@ use std::sync::mpsc;
 use std::sync::mpsc::Receiver;
 use std::sync::mpsc::Sender;
 use std::{fs, thread};
-// struct CPU
-// field:
-// cycle: usize
-// x_register: i32
-
-// method:
-// execute(instruction: str)
-// case addx:
-// cycle += 1
-// send (cycle, x_register) message
-// cycle += 1
-// x_register is new value
-// send (cycle, x_register) message
-// emit cycle:
-// case noop:
-// cycle += 1
-// send (cycle, x_register) message
-
-// I wanna use threads
-// std::sync::mpsc guarantees that messages sent by the sender will be received in order by the receiver.
-
-// Main transmitter Thread:
-// create the mpsc channel
-// spawns the receiver thread
-// and give the receiver thread ownership of receiver on mpsc channel
-
-// creates CPU struct
-// reads lines from file,
-// CPU struct will send to receiver (cycle, x_register) tuple during execute() of those lines.
-
-// wait for the receiver thread to close.
-
-// Signal Strength receiver Thread:
-// for each item received:
-// receives tuple of (usize, i32) representing the cycle and register value.
-// signal strength = 0.
-// if cycle is >= 220
-// break out
-// sets i = 0.
-// if cycle value is % 20 + 40i.
-// increment i by 1
-// add register value to signal strength
 
 struct CPU {
     cycle: usize,
@@ -97,7 +55,7 @@ fn main() {
 
     let mut cpu = CPU::new(tx);
 
-    let receiver_handle = thread::spawn(move || signal_receiver(rx));
+    let receiver_handle = thread::spawn(move || signal_strength_receiver(rx));
 
     for instruction in cpu_instructions.lines() {
         if cpu.cycle > 220 {
@@ -112,9 +70,18 @@ fn main() {
     receiver_handle.join().unwrap()
 }
 
-fn signal_receiver(receiver: Receiver<(usize, i32)>) {
+fn signal_strength_receiver(receiver: Receiver<(usize, i32)>) {
+    let mut signal_strength = 0;
+    let mut i = 0;
+
     for cpu_state in receiver {
         let (cycle, x_register) = cpu_state;
-        todo!()
+
+        if cycle % 20 + 40 * i == 0 {
+            signal_strength += x_register;
+            i += 1;
+        }
     }
+
+    println!("Final Signal Strength: {}", signal_strength);
 }
